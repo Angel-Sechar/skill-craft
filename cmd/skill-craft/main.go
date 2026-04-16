7cebe518ba9b4150e7a8a1d699776de806b2df91
Angel Sechar <asv70537592@gmail.com>
2026-04-16 06:12:59 -0500

    fix: read version from build info for go install

--- cmd/skill-craft/main.go
+++ cmd/skill-craft/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/Angel-Sechar/skill-craft/internal/tui"
 )
@@ -11,7 +12,7 @@ var version = "dev"
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Println("skill-craft v" + version)
+		fmt.Println("skill-craft v" + getVersion())
 		return
 	}
 
@@ -19,4 +20,16 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func getVersion() string {
+	if version != "dev" {
+		return version
+	}
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+	return version
 }
